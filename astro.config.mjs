5448cbc45705dbc524c1a57155f30a8fb29fd8d4
Youngbin Han <ybhan@ubuntu.com>
2025-06-02 00:33:47 +0900

    mdx 지원 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
   i18n: {
     locales: ["ko", "en"],
     defaultLocale: "ko",
