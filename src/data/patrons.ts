2d4ee66c1368cf1857ace11e38f4c9956fa2fa41
Youngbin Han <ybhan@ubuntu.com>
2025-09-19 16:01:56 +0900

    Update patrons.ts

--- src/data/patrons.ts
+++ src/data/patrons.ts
@@ -4,5 +4,10 @@ export const PatronList = [
     {
         name: "노하은",
         affiliation: "주식회사유니브"
+    },
+    {
+        name: "김석현",
+        affiliation: "한국 스프링 사용자 모임(KSUG)"
     }
 ];
+
