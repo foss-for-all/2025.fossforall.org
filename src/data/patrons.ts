0c44f9c882de8b34a2c2c7ee15d196648155772e
Youngbin Han <ybhan@ubuntu.com>
2025-09-26 01:11:24 +0900

    개인후원자 목록 갱신

--- src/data/patrons.ts
+++ src/data/patrons.ts
@@ -1,13 +1,18 @@
-
-
 export const PatronList = [
-    {
-        name: "노하은",
-        affiliation: "주식회사유니브"
-    },
-    {
-        name: "김석현",
-        affiliation: "한국 스프링 사용자 모임(KSUG)"
-    }
+  {
+    name: "노하은",
+    affiliation: "주식회사유니브",
+  },
+  {
+    name: "김석현",
+    affiliation: "한국 스프링 사용자 모임(KSUG)",
+  },
+  {
+    name: "김종민",
+    affiliation: "Debian",
+  },
+  {
+    name: "한영빈",
+    affiliation: "FOSS for All",
+  },
 ];
-
