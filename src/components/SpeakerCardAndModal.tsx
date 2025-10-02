749b710ba4564bdb90256c456ce68ba09467e891
Youngbin Han <ybhan@ubuntu.com>
2025-10-02 22:26:54 +0900

    발표자 세부 정보 모달 표시

---
+++ src/components/SpeakerCardAndModal.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import type { Speaker } from "@/models/Speaker";
+import { sliceStringByBytes } from "@/lib/utils";
+
+interface SpeakerCardAndModalProps {
+  speaker: Speaker;
+  gravatarHash: string;
+}
+
+export function SpeakerCardAndModal({
+  speaker,
+  gravatarHash,
+}: SpeakerCardAndModalProps) {
+  const [isActive, setIsActive] = useState(false);
+
+  function openModal() {
+    setIsActive(true);
+  }
+
+  function closeModal() {
+    setIsActive(false);
+  }
+
+  return (
+    <div className="column is-one-quarter">
+      <div className="card" onClick={openModal}>
+        <div className="card-image">
+          <figure className="image is-square">
+            <img
+              style={{ objectFit: "cover" }}
+              src={
+                speaker.avatar_url == ""
+                  ? `https://gravatar.com/avatar/${gravatarHash}?s=200`
+                  : speaker.avatar_url
+              }
+              alt={speaker.name}
+            />
+          </figure>
+        </div>
+        <div className="card-content">
+          <p className="title is-4">{speaker.name}</p>
+          <div className="content">
+            {sliceStringByBytes(speaker.biography, 0, 100)}...
+          </div>
+        </div>
+      </div>
+      <div className={`modal ${isActive ? "is-active" : ""}`}>
+        <div className="modal-background" onClick={closeModal}></div>
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title"></p>
+            <button
+              className="delete"
+              aria-label="close"
+              onClick={closeModal}
+            ></button>
+          </header>
+          <section className="modal-card-body">
+            <figure className="image is-square">
+              <img
+                style={{ objectFit: "cover" }}
+                src={
+                  speaker.avatar_url == ""
+                    ? `https://gravatar.com/avatar/${gravatarHash}?s=200`
+                    : speaker.avatar_url
+                }
+                alt={speaker.name}
+              />
+            </figure>
+            <h2>{speaker.name}</h2>
+            <div dangerouslySetInnerHTML={{ __html: speaker.biography }}></div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/models/Speaker.ts
@@ -0,0 +1,12 @@
+export interface Speaker {
+  code: string;
+  name: string;
+  biography: string;
+  submissions: string[];
+  avatar_url: string;
+  answers: string[];
+  email: string;
+  timezone: string;
+  locale: string;
+  has_arrived: boolean;
+}
