9a999455f3e499fd5ab2157306e4c5b26a7d701e
Sungjoon Moon <sumoon@seoulsaram.org>
2025-10-19 02:36:47 +0900

    data: update speaker organizations
    Adds new speaker organizations to the list.

--- src/data/speakerOrganizations.ts
+++ src/data/speakerOrganizations.ts
@@ -8,4 +8,16 @@ export const SpeakerOrganizations = [
     name: "RichiH",
     organization: "FOSDEM",
   },
+  {
+    name: "김종민",
+    organization: "Debian",
+  },
+  {
+    name: "김윤성",
+    organization: "Debian",
+  },
+  {
+    name: "이성민 (boltless)",
+    organization: "tangled.org",
+  },
 ];
